8ccb188b756d06d8a6946873d7a212adc8353ed2
willbill0808 <williamlrosewell08@gmail.com>
2026-01-07 16:53:47 +0100

    redone first

---
+++ redone/out_sourcing/installs.fish
@@ -0,0 +1,50 @@
+#!/usr/bin/env fish
+
+cd ~
+
+if not grep -q '^\[multilib\]' /etc/pacman.conf
+    echo "==> Enabling multilib repository..."
+    sudo sed -i '/#\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+else
+    echo "==> Multilib already enabled."
+end
+
+# Base packages
+sudo pacman -Syu unzip nautilus vscode rofi gnome-keyring base-devel git steam fastfetch btop --noconfirm; or exit 1
+set_color green; echo "done with pacman"; set_color normal
+
+# Snap
+if not test -d ~/snapd
+    git clone https://aur.archlinux.org/snapd.git
+end
+cd snapd
+makepkg -si; or exit 1
+sudo systemctl enable --now snapd.socket
+sudo ln -sf /var/lib/snapd/snap /snap
+cd ..
+set_color green; echo "done with snap download"; set_color normal
+
+# Yay
+if not test -d ~/yay
+    git clone https://aur.archlinux.org/yay.git
+end
+cd yay
+makepkg -si; or exit 1
+cd ..
+set_color green; echo "done with yay download"; set_color normal
+
+# Yay packages
+yay -S librewolf-bin github-desktop --noconfirm
+set_color green; echo "done with yay"; set_color normal
+
+github-desktop &
+
+
+read -P "Do you want to reboot(best for clean install)? (y/n) " answer
+
+if string match -qi y $answer
+    echo "User chose yes"
+    reboot
+else
+    echo "User chose no"
+end
