150d95c9ed874fb0deb7d77015d1209d1df41b70
willbill0808 <williamlrosewell08@gmail.com>
2026-01-08 00:13:47 +0100

    rofi

--- redone/out_sourcing/installs.fish
+++ redone/out_sourcing/installs.fish
@@ -13,6 +13,8 @@ end
 sudo pacman -Syu swww  zsh swaync unzip nautilus vscode rofi gnome-keyring base-devel git steam fastfetch btop --noconfirm; or exit 1
 set_color green; echo "done with pacman"; set_color normal
 
+swww img Documents/hyprland_setup/redone/wallpapers/wallpaper.jpg
+
 # Yay
 if not test -d ~/yay
     git clone https://aur.archlinux.org/yay.git
