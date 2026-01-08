c04106fdde9a7f7ea319b7380564bb950a24d934
willbill0808 <williamlrosewell08@gmail.com>
2026-01-08 16:41:49 +0100

    Update setup_redone.sh

--- setup_redone.sh
+++ setup_redone.sh
@@ -8,11 +8,6 @@ sudo pacman -Syu zsh steam fish base-devel git gnome-keyring \
   hyprlock hypridle hyprpaper waybar spotify-launcher swww swaync \
   unzip nautilus vscode rofi fastfetch btop  
 
-if [[ -n "$WAYLAND_DISPLAY" || -n "$DISPLAY" ]]; then
-  eval "$(gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)" >/dev/null 2>&1 &
-  export SSH_AUTH_SOCK
-fi
-
 git clone https://aur.archlinux.org/yay.git
 
 cd yay
@@ -36,10 +31,6 @@ mkdir -p ~/.config/hypr
 cp hyprland.conf hyprlock.conf hyprpaper.conf ~/.config/hypr/
 
 cd ~
-
-systemctl --user enable gnome-keyring-daemon.service
-systemctl --user enable gnome-keyring-daemon.socket
-systemctl --user enable gnome-keyring-daemon-ssh.socket
 
 swww-daemon >/dev/null 2>&1 &
 swww img ~/Documents/hyprland_setup/redone/wallpapers/wallpaper.jpg
